f6d52b9c6c34097620146464dc6d92ff7c4149f5
hananeh.khoram <hananeh.khoram@gmail.com>
2026-06-10 22:03:21 +0330

    phase_0

--- model/zombie/ability/Ability.java
+++
@@ -1,8 +0,0 @@
-package model.zombie.ability;
-
-import model.zombie.Zombie;
-
-public interface Ability {
-    void execute(Zombie zombie);
-}
-
--- model/zombie/ability/ImpThrowingAbility.java
+++
@@ -1,9 +0,0 @@
-package model.zombie.ability;
-import model.zombie.Zombie;
-
-public class ImpThrowingAbility implements Ability {
-    @Override
-    public void execute(Zombie zombie) {
-        }
-}
-
