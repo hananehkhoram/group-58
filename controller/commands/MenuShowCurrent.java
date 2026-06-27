cdf6a3d335b6e44b3e5711d5f30a1dd5071a97aa
Melody Halaj <melodyyy12855@gmail.com>
2026-06-27 23:15:53 +0330

    debug

--- controller/commands/MenuShowCurrent.java
+++ controller/commands/MenuShowCurrent.java
@@ -3,6 +3,7 @@ package controller.commands;
 import controller.MenuManager;
 import controller.commandHandler.Command;
 import model.menus.Menu;
+import view.ConsoleView;
 
 public class MenuShowCurrent implements Command {
     private MenuManager menuManager;
@@ -15,6 +16,7 @@ public class MenuShowCurrent implements Command {
     public void execute(String[] atgs ){
 
         Menu currentMenu = this.menuManager.getCurrentMenu();
+        ConsoleView.showMessage(currentMenu.toString());
 
     }
 
