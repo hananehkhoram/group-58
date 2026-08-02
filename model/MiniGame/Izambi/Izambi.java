8215ebf4d53c47d6d8e1fa67a70a703e36b989a5
tarane <tarane.mdmd@gmail.com>
2026-08-02 14:50:20 +0330

    Debug

--- model/MiniGame/Izambi/Izambi.java
+++ model/MiniGame/Izambi/Izambi.java
@@ -52,10 +52,7 @@ public class Izambi {
         this.sunAmount = 150;
         ctx.setSunAmount(this.sunAmount);
 
-        // قرار دادن زامبی‌های تولیدکننده خورشید در ستون اول هر ردیف
         initSunProducerZombies();
-        // قرار دادن مغزها در انتهای ردیف‌ها
-        initBrains();
 
         System.out.print("start\n");
     }
@@ -107,19 +104,18 @@ public class Izambi {
         int rows = currentLevel.getRows();
 
         PlantFactory plantFactory = new PlantFactory(DataManager.getInstance());
+
         for (int r = 0; r < rows; r++) {
-            Plant sunPlant = plantFactory.create("Sunflower"); // یا نام گیاه تولیدکننده خورشید در بازی شما
-            if (sunPlant != null) {
-                // گرفتن کاشی در ستون صفر و سطر r
-                Tile tile = ctx.getGameEngine().getTiles(0, r);
-                if (tile != null) {
-                    tile.setPlant(sunPlant);
+            for (int c = 0; c < 6; c++) {
+                Plant sunPlant = plantFactory.create("Sunflower"); // یا گیاه دلخواه برای این ستون‌ها
+                if (sunPlant != null) {
+                    Tile tile = ctx.getGameEngine().getTiles(c, r);
+                    if (tile != null) {
+                        tile.setPlant(sunPlant);
+                    }
                 }
             }
         }
-    }
-    private void initBrains() {
-        // پیاده‌سازی مغزها در انتهای ردیف‌ها مطابق با قوانین پروژه
     }
 
     public GameContext getCtx(){
@@ -132,13 +128,10 @@ public class Izambi {
     public void advancedTimeCommand(double sec){
         if (this.gameEngine != null) {
             this.gameEngine.update(sec);
-            checkWinLossConditions();
         } else {
             ConsoleView.showMessage("Game engine is null");
         }
     }
 
-    private void checkWinLossConditions() {
-        // بررسی شرایط برد (خورده شدن تمام مغزها) و باخت (تمام شدن زامبی‌ها و نداشتن خورشید کافی)
-    }
+
 }
