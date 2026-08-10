cae1b18090dc0f49341d2018edb9ff356221bcdf
tarane <tarane.mdmd@gmail.com>
2026-08-11 02:48:14 +0330

    SettingsMenu

--- core/src/main/java/com/workshop/view/Screens/SettingsScreen.java
+++ core/src/main/java/com/workshop/view/Screens/SettingsScreen.java
@@ -3,103 +3,276 @@ package com.workshop.view.Screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.CheckBox;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.SelectBox;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.utils.Scaling;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
+
 import com.workshop.PvzGame;
 import com.workshop.controller.repository.DataManager;
 import com.workshop.model.user.User;
+
 import pvz.skin.PvzSkin;
 
 public class SettingsScreen implements Screen {
 
     private final Stage stage;
+    private Texture backgroundTexture;
 
     public SettingsScreen(PvzGame game, User user) {
         Skin skin = PvzSkin.get();
         stage = new Stage(new ScreenViewport());
 
+        buildBackground();
+        buildUi(game, user, skin);
+    }
+
+    private void buildBackground() {
+        backgroundTexture = new Texture(
+            Gdx.files.internal(
+                "IMAGES/Menus/Settings/SettingsBackground.png"
+            )
+        );
+
+        Image background = new Image(backgroundTexture);
+        background.setScaling(Scaling.fill);
+        background.setFillParent(true);
+
+        stage.addActor(background);
+    }
+
+    private void buildUi(PvzGame game, User user, Skin skin) {
         Table root = new Table();
         root.setFillParent(true);
-        root.center();
+
+        root.left();
+        root.padLeft(180);
+
         stage.addActor(root);
 
-        Label title = new Label("Settings", skin, "big");
+        Table settingsPanel = new Table();
 
-        SelectBox<Integer> difficultyBox = new SelectBox<>(skin);
-        difficultyBox.setItems(1, 2, 3, 4, 5);
-        difficultyBox.setSelected(user.getDifficultyLevel());
+        Label title = new Label(
+            "Settings",
+            skin,
+            "big"
+        );
 
-        SelectBox<Integer> gameSpeedBox = new SelectBox<>(skin);
-        gameSpeedBox.setItems(1, 2, 3);
-        gameSpeedBox.setSelected(user.getGameSpeed());
+        SelectBox<Integer> difficultyBox =
+            new SelectBox<>(skin);
 
-        CheckBox gridCheckBox = new CheckBox(" Show Grid", skin);
-        gridCheckBox.setChecked(user.isGridEnabled());
+        difficultyBox.setItems(
+            1,
+            2,
+            3,
+            4,
+            5
+        );
 
-        CheckBox debugCheckBox = new CheckBox(" Debug Mode", skin);
-        debugCheckBox.setChecked(user.isDebugMode());
+        difficultyBox.setSelected(
+            user.getDifficultyLevel()
+        );
 
-        TextButton backButton = new TextButton("Back", skin, "default");
+        SelectBox<Integer> gameSpeedBox =
+            new SelectBox<>(skin);
 
-        backButton.addListener(new ChangeListener() {
-            @Override
-            public void changed(ChangeEvent event, Actor actor) {
-                DataManager.getInstance().saveUser();
-                game.showMain();
-            }
-        });
+        gameSpeedBox.setItems(
+            1,
+            2,
+            3
+        );
 
-        difficultyBox.addListener(new ChangeListener() {
-            @Override
-            public void changed(ChangeEvent event, Actor actor) {
-                user.setDifficultyLevel(difficultyBox.getSelected());
-            }
-        });
+        gameSpeedBox.setSelected(
+            user.getGameSpeed()
+        );
 
-        gameSpeedBox.addListener(new ChangeListener() {
-            @Override
-            public void changed(ChangeEvent event, Actor actor) {
-                user.setGameSpeed(gameSpeedBox.getSelected());
-            }
-        });
+        CheckBox gridCheckBox =
+            new CheckBox(
+                " Show Grid",
+                skin
+            );
 
-        gridCheckBox.addListener(new ChangeListener() {
-            @Override
-            public void changed(ChangeEvent event, Actor actor) {
-                user.setGridEnabled(gridCheckBox.isChecked());
-            }
-        });
+        gridCheckBox.setChecked(
+            user.isGridEnabled()
+        );
 
-        debugCheckBox.addListener(new ChangeListener() {
-            @Override
-            public void changed(ChangeEvent event, Actor actor) {
-                user.setDebugMode(debugCheckBox.isChecked());
-            }
-        });
+        CheckBox debugCheckBox =
+            new CheckBox(
+                " Debug Mode",
+                skin
+            );
 
-        root.add(title).colspan(2).padBottom(30).row();
+        debugCheckBox.setChecked(
+            user.isDebugMode()
+        );
 
-        root.add(new Label("Difficulty", skin)).pad(10);
-        root.add(difficultyBox).width(150).pad(10).row();
+        TextButton backButton =
+            new TextButton(
+                "Back",
+                skin,
+                "default"
+            );
 
-        root.add(new Label("Game Speed", skin)).pad(10);
-        root.add(gameSpeedBox).width(150).pad(10).row();
+        addListeners(
+            game,
+            user,
+            difficultyBox,
+            gameSpeedBox,
+            gridCheckBox,
+            debugCheckBox,
+            backButton
+        );
 
-        root.add(gridCheckBox).colspan(2).pad(10).row();
-        root.add(debugCheckBox).colspan(2).pad(10).row();
+        settingsPanel.add(title)
+            .colspan(2)
+            .padBottom(35)
+            .row();
 
-        root.add(backButton)
+        settingsPanel.add(
+                new Label(
+                    "Difficulty",
+                    skin
+                )
+            )
+            .pad(10)
+            .right();
+
+        settingsPanel.add(difficultyBox)
+            .width(150)
+            .pad(10)
+            .row();
+
+        settingsPanel.add(
+                new Label(
+                    "Game Speed",
+                    skin
+                )
+            )
+            .pad(10)
+            .right();
+
+        settingsPanel.add(gameSpeedBox)
+            .width(150)
+            .pad(10)
+            .row();
+
+        Table checkBoxTable = new Table();
+
+        checkBoxTable.add(gridCheckBox)
+            .left()
+            .row();
+
+        checkBoxTable.add(debugCheckBox)
+            .left()
+            .padTop(12)
+            .row();
+
+        settingsPanel.add(checkBoxTable)
+            .colspan(2)
+            .left()
+            .padLeft(65)
+            .padTop(15)
+            .padBottom(15)
+            .row();
+
+        settingsPanel.add(backButton)
             .colspan(2)
             .width(200)
-            .padTop(30);
+            .height(55)
+            .padTop(25);
+
+        root.add(settingsPanel);
+    }
+
+    private void addListeners(
+        PvzGame game,
+        User user,
+        SelectBox<Integer> difficultyBox,
+        SelectBox<Integer> gameSpeedBox,
+        CheckBox gridCheckBox,
+        CheckBox debugCheckBox,
+        TextButton backButton
+    ) {
+        difficultyBox.addListener(
+            new ChangeListener() {
+                @Override
+                public void changed(
+                    ChangeEvent event,
+                    Actor actor
+                ) {
+                    user.setDifficultyLevel(
+                        difficultyBox.getSelected()
+                    );
+                }
+            }
+        );
+
+        gameSpeedBox.addListener(
+            new ChangeListener() {
+                @Override
+                public void changed(
+                    ChangeEvent event,
+                    Actor actor
+                ) {
+                    user.setGameSpeed(
+                        gameSpeedBox.getSelected()
+                    );
+                }
+            }
+        );
+
+        gridCheckBox.addListener(
+            new ChangeListener() {
+                @Override
+                public void changed(
+                    ChangeEvent event,
+                    Actor actor
+                ) {
+                    user.setGridEnabled(
+                        gridCheckBox.isChecked()
+                    );
+                }
+            }
+        );
+
+        debugCheckBox.addListener(
+            new ChangeListener() {
+                @Override
+                public void changed(
+                    ChangeEvent event,
+                    Actor actor
+                ) {
+                    user.setDebugMode(
+                        debugCheckBox.isChecked()
+                    );
+                }
+            }
+        );
+
+        backButton.addListener(
+            new ChangeListener() {
+                @Override
+                public void changed(
+                    ChangeEvent event,
+                    Actor actor
+                ) {
+                    DataManager
+                        .getInstance()
+                        .saveUser();
+
+                    game.showMain();
+                }
+            }
+        );
     }
 
     @Override
@@ -110,33 +283,50 @@ public class SettingsScreen implements Screen {
     @Override
     public void render(float delta) {
         Gdx.gl.glClearColor(
-            95f / 255f,
-            59f / 255f,
-            35f / 255f,
+            0f,
+            0f,
+            0f,
             1f
         );
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        Gdx.gl.glClear(
+            GL20.GL_COLOR_BUFFER_BIT
+        );
 
         stage.act(delta);
         stage.draw();
     }
 
     @Override
-    public void resize(int width, int height) {
-        stage.getViewport().update(width, height, true);
+    public void resize(
+        int width,
+        int height
+    ) {
+        stage.getViewport().update(
+            width,
+            height,
+            true
+        );
     }
 
     @Override
-    public void pause() {}
+    public void pause() {
+    }
 
     @Override
-    public void resume() {}
+    public void resume() {
+    }
 
     @Override
-    public void hide() {}
+    public void hide() {
+    }
 
     @Override
     public void dispose() {
         stage.dispose();
+
+        if (backgroundTexture != null) {
+            backgroundTexture.dispose();
+        }
     }
 }
